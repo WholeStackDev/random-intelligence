e68b5cb7bb7656c396df719773b9034f9c5ea66b
Michael Phillips <wholestackdev@gmail.com>
2019-11-02 18:12:55 -0400

    Did stuff

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
 import styles from "./style.module.css";
-import dict from "./words_dictionary.json";
+import dict from "./websters.json";
 
 const getRandomNumber = (min, max) => {
   return Math.floor(Math.random() * (max + 1 - min) + min);
@@ -9,75 +9,177 @@ const getRandomNumber = (min, max) => {
 
 function App() {
   const spaceProbability = 0.25;
-  let [resultsSampleArray, setResultsSampleArray] = useState([]);
+
+  const [resultsSampleArray, setResultsSampleArray] = useState([]);
+
+  const [stringLength, setStringLength] = useState(45);
+
+  const [reportInterval, setReportInterval] = useState(1000);
+  const [multiLetterFactor, setMultiLetterFactor] = useState(1.5);
+  const [topListMaxLength, setTopListMaxLength] = useState(20);
+
+  const [topList, setTopList] = useState([]);
+  const topListRef = useRef(topList);
+  topListRef.current = topList;
+
   const [runs, setRuns] = useState(0);
-  const [totalValidCharacters, setTotalValidCharacters] = useState(0);
+  const runsRef = useRef(runs);
+  runsRef.current = runs;
 
-  const run = () => {
-    let test = dict['test'];
-    // const dict = JSON.parse(dictJSON);
-  // let test = dict['aa'];
-    let count = 0;
-    const resultsArray = [];
+  const [isRunning, setIsRunning] = useState(false);
+  const isRunningRef = useRef(isRunning);
+  isRunningRef.current = isRunning;
 
-    let currentString = "";
-    let currentTrimmedString = "";
-    let currentStringArray = [];
-    let currentValidWords = 0;
-    let currentInvalidWords = 0;
-    let currentSequentialWords = 0;
-    let currentValidCharacters = 0;
-    let runningValidCharacters = 0;
+  const [bestResult, setBestResult] = useState({ score: 0, string: "" });
+  const bestResultRef = useRef(bestResult);
+  bestResultRef.current = bestResult;
 
-    for (let step = 1; step <= 10; step++) {
-      currentValidWords = 0;
-      currentInvalidWords = 0;
-      currentString = "";
-      currentValidCharacters = 0;
-
-      for (let step = 1; step <= 50; step++) {
-        if (Math.random() < spaceProbability) {
-          currentString += " ";
-        } else {
-          currentString += String.fromCharCode(getRandomNumber(65, 90));
-        }
-      }
-      currentTrimmedString = currentString.replace(/\s+/g, " ").trim();
-      currentStringArray = currentTrimmedString.split(" ");
-
-      currentStringArray.forEach(str => {
-        if (dict[str]) {
-          currentValidCharacters += str.length;
-          currentValidWords++;
-        } else {
-          currentInvalidWords++;
-        }
-      });
-      resultsArray.push(currentTrimmedString);
-      runningValidCharacters += currentValidCharacters;
-      count++;
+  useEffect(() => {
+    if (isRunning) {
+      loop();
     }
-    setRuns(runs + count);
-    setResultsSampleArray([...resultsSampleArray, ...resultsArray]);
-    setTotalValidCharacters(runningValidCharacters);
+  }, [isRunning]);
+
+  const startRunning = () => {
+    setIsRunning(true);
   };
 
-  const ResultsSample = () => {
-    const listItems = resultsSampleArray.map(str => <p>{str}</p>);
+  const stopRunning = () => {
+    setIsRunning(false);
+  };
+
+  const loop = () => {
+    if (isRunningRef.current) {
+      multipleAttempts(reportInterval);
+      setTimeout(loop, 20);
+    }
+  };
+
+  const multipleAttempts = attempts => {
+    let earlyExit = false;
+    for (let step = 1; step <= attempts; step++) {
+      let newScore = singleAttempt();
+      if (newScore) {
+        setRuns(runsRef.current + step);
+        earlyExit = true;
+        break;
+      }
+    }
+    if (earlyExit === false) {
+      setRuns(runsRef.current + reportInterval);
+    }
+  };
+
+  const singleAttempt = () => {
+    let currentString = "";
+
+    for (let step = 1; step <= stringLength; step++) {
+      if (Math.random() < spaceProbability) {
+        currentString += " ";
+      } else {
+        currentString += String.fromCharCode(getRandomNumber(65, 90));
+      }
+    }
+
+    let currentTrimmedString = currentString.replace(/\s+/g, " ").trim();
+    let currentStringArray = currentTrimmedString.split(" ");
+
+    let currentScore = 0;
+
+    currentStringArray.forEach(str => {
+      if (dict[str]) {
+        currentScore += str.length ^ multiLetterFactor;
+      }
+    });
+    if (topListRef.current.length < topListMaxLength || currentScore > topListRef.current[topListRef.current.length - 1].score) {
+      addToTopList({
+        score: currentScore,
+        string: currentTrimmedString
+      });
+      return true;
+    }
+    return false;
+  };
+
+  const addToTopList = newTrial => {
+    let newTrialSplit = newTrial.string.split(" ");
+    let newTrialWords = [];
+    newTrialSplit.forEach(str => {
+      let isWord = dict[str] ? true : false;
+      newTrialWords.push({ word: str, isWord: isWord });
+    });
+    newTrial.words = newTrialWords;
+
+    let added = [...topListRef.current, newTrial];
+    let sorted = added.sort(compareScore);
+    sorted.splice(topListMaxLength, 5);
+    setTopList(sorted);
+  };
+
+  const compareScore = (a, b) => {
+    if (a.score < b.score) {
+      return 1;
+    }
+    if (a.score > b.score) {
+      return -1;
+    }
+    return 0;
+  };
+
+  const TopScores = () => {
+    const listItems = topList.map(str => (
+      <p key={str.string}>
+        {DisplayString(str)}
+        <span> - </span>
+        <span>{str.score}</span>
+      </p>
+    ));
     return <div className={styles.results}>{listItems}</div>;
+  };
+
+  const DisplayString = str => {
+    const spans = str.words.map((word, index) => (
+      <span key={index} style={word.isWord ? { fontWeight: "900" } : { fontWeight: "400" }}>
+        {word.word}{" "}
+      </span>
+    ));
+    return spans;
+  };
+
+  const DisplayRuns = () => {
+    return <p>{runs.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>;
   };
 
   return (
     <div className="App">
-      <button onClick={run}>Run</button>
-      <h2>Summary</h2>
+      <h1>Random Intelligence</h1>
+      <h2 className="subtitle">A simulation to conceptualize the difficulty of evolution</h2>
+      <p>
+        We've all been told that all life came about purely by random chance. This application is designed to illustrate the
+        improbability of that assertion.
+      </p>
+      <p>
+        When you hit start, your computer will begin generating strings roughly 50 characters in length. There is a 25%
+        probability of getting a space rather than a character and all extra spaces are removed (we'll cheat like that just a bit
+        to give evolution a hand). Each string will be checked for words against the webster's dictionary and the best ones will
+        be shown.
+      </p>
+      <p>
+        Note: It starts off very slow (because the ranking part is slow), but it speeds up a ton when it isn't updating the top
+        scorers every other second.
+      </p>
+      {/* <button onClick={run}>Run</button> */}
+      <button onClick={startRunning}>Start Running</button>
+      <button onClick={stopRunning}>Stop Running</button>
+      {/* <h2>Summary</h2>
       <p>
         <span>Valid Characters</span>
         <span>{totalValidCharacters}</span>
       </p>
       <h2>Results Sample</h2>
-      <ResultsSample />
-      <p>{runs}</p>
+      <ResultsSample /> */}
+      <DisplayRuns />
+      <TopScores />
     </div>
   );
 }
